fd7a018ad4a37de5c8d3daa2e4c776a7194afccc
BolgaroVV <khasanov.alexey999@gmail.com>
2024-07-04 00:44:33 +0500

    third commit

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let a = document.querySelector('.about-me-content')
+let a = document.querySelector('[name="about-me"]')
 let b = document.querySelector('.block_title').querySelectorAll('div')
 let c = 0
 for (const i of b) {
-    console.log(i.offsetWidth)
     c += i.offsetWidth
 }
-a.style.width = c + 'px'
+a.style.maxWidth = c + 'px'
