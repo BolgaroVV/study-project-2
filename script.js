c91ff681e11628e5ba619fd4c7b6b8bd19fe7adf
BolgaroVV <khasanov.alexey999@gmail.com>
2024-07-05 15:38:08 +0500

    finished page without adaptive

--- script.js
+++
@@ -1,7 +0,0 @@
-let a = document.querySelector('[name="about-me"]')
-let b = document.querySelector('.block_title').querySelectorAll('div')
-let c = 0
-for (const i of b) {
-    c += i.offsetWidth
-}
-a.style.maxWidth = c + 'px'
